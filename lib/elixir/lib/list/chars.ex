f62cb99180a3f8096025af4f8861f0589fbc835e
Jay Hayes <ur@iamvery.com>
2016-10-13 16:54:47 -0500

    Replace List.Chars protocol with module implementation

--- lib/elixir/lib/list/chars.ex
+++ lib/elixir/lib/list/chars.ex
@@ -1,4 +1,4 @@
-defprotocol List.Chars do
+defmodule List.Chars do
   @moduledoc ~S"""
   The List.Chars protocol is responsible for
   converting a structure to a list (only if applicable).
@@ -9,48 +9,38 @@ defprotocol List.Chars do
   by `Kernel` invokes this protocol.
   """
 
-  def to_charlist(term)
+  @callback to_charlist(term) :: []
 
-  # TODO: Deprecate by v1.5
-  @doc false
-  Kernel.def to_char_list(term) do
-    __MODULE__.to_charlist(term)
+  def to_charlist(%{__struct__: module} = struct) do
+    module.to_charlist(struct)
   end
-end
 
-defimpl List.Chars, for: Atom do
-  def to_charlist(atom), do: Atom.to_charlist(atom)
-end
+  def to_charlist(atom) when is_atom(atom), do: Atom.to_charlist(atom)
+  # Note that same inlining is used for the rewrite rule.
+  def to_charlist(list) when is_list(list), do: list
 
-defimpl List.Chars, for: BitString do
-  @doc """
-  Returns the given binary `term` converted to a charlist.
-  """
+  def to_charlist(term) when is_integer(term) do
+    Integer.to_charlist(term)
+  end
+
+  def to_charlist(term) when is_float(term) do
+    :io_lib_format.fwrite_g(term)
+  end
+
   def to_charlist(term) when is_binary(term) do
     String.to_charlist(term)
   end
 
   def to_charlist(term) do
     raise Protocol.UndefinedError,
-             protocol: @protocol,
+             protocol: __MODULE__,
                 value: term,
           description: "cannot convert a bitstring to a charlist"
   end
-end
 
-defimpl List.Chars, for: List do
-  # Note that same inlining is used for the rewrite rule.
-  def to_charlist(list), do: list
-end
-
-defimpl List.Chars, for: Integer do
-  def to_charlist(term) do
-    Integer.to_charlist(term)
-  end
-end
-
-defimpl List.Chars, for: Float do
-  def to_charlist(term) do
-    :io_lib_format.fwrite_g(term)
+  # TODO: Deprecate by v1.5
+  @doc false
+  Kernel.def to_char_list(term) do
+    __MODULE__.to_charlist(term)
   end
 end
